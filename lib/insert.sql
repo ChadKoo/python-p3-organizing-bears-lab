89a1bb59df965af493bde2d853f81995ed9c0c4a
Chadwick Koo <chadwickkoo@outlook.com>
2025-05-21 13:28:50 +0300

    Modified files

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "black", "angry", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "brown", "playful", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "white", "angry", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "black", "angry", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "brown", "sad", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "F", "black", "playful", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "M", "brown", "sad", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, "F", "black", "angry", true);
